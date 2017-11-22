de5d8fec442ba490556a32b3c152ed37a7d958e3
MarkV43 <marcelinho.vogt@gmail.com>
2017-11-21 22:21:57 -0200

    Tileset Corrigido

--- src/levels/tilemap2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset2" tilewidth="64" tileheight="64" tilecount="25" columns="5">
- <image source="../tilemaps/tilemap2.png" width="320" height="320"/>
-</tileset>
--- src/tilemaps/tileset2.tsx
+++ src/tilemaps/tileset2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset2" tilewidth="16" tileheight="16" tilecount="20" columns="4">
- <image source="tilemap2.png" width="64" height="80"/>
+<tileset name="tileset2" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="tilemap2.png" width="320" height="320"/>
 </tileset>
