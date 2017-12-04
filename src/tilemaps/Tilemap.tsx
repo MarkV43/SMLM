a05cc668fa563422b05e4043579ded81f2544a48
Gerson Costa <gersonkatumata@gmail.com>
2017-12-04 20:24:22 -0200

    !

--- src/tilemaps/Tilemap.tsx
+++ src/tilemaps/Tilemap.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="TileMap" tilewidth="64" tileheight="64" tilecount="20" columns="4">
- <image source="../tilemaps/TileMap.png" width="256" height="320"/>
+ <image source="TileMap.png" width="256" height="320"/>
 </tileset>
